e818c34e7471f096e0e70797ab187deaf7c5df54
JohnWilliams1990 <JohnWilliams199032@gmail.com>
2018-06-05 21:25:45 -0600

    Manipulation of directories and removing of files

--- cleanup.rb
+++ cleanup.rb
@@ -1,13 +1,39 @@
 
+# This recipy is used to demonstare simple manipulation of direcotries and 
+# the use of making files in Relative paths.
 
-
-file "#{ENV['HOME']}/Desktop/oreilly/chapter4Recipies/stone.txt" do 
+file "#{ENV['HOME']}/Desktop/oreilly/chapter4/output\ files/stone.txt" do 
 	action :delete
 end 
 
 
-file "#{ENV['HOME']}/Desktop/oreilly/chapter4Recipies/hello.txt" do
+file "#{ENV['HOME']}/Desktop/oreilly/chapter4/output\ files/hello.txt" do
+        action :delete
+end
+
+directory "#{ENV['HOME']}/Desktop/oreilly/chapter4/output\ files/" do
         action :delete
 end
 
 
+0.upto(10) do |i|
+
+	directory "#{ENV['HOME']}/Desktop/oreilly/chapter#{i}" do
+		action :delete
+		ignore_failure true
+	end
+end
+
+#
+#1.upto(10) do |i|
+#
+##	directory "#{ENV['HOME']}/Desktop/oreilly/chapter#{i}" do
+#       	action :delete
+#       	ignore_failure true
+#       end
+#end
+#
+##
+##
+#
+#
--- stone.rb
+++ stone.rb
@@ -1,11 +1,25 @@
 
+1.upto(10) do |i|
+	directory "#{ENV['HOME']}/Desktop/oreilly/chapter#{i}" do 
+		action :create
+	end 
+end
+
+directory "#{ENV['HOME']}/Desktop/oreilly/chapter4/output\ files" do 
+		action :create
+	end 
 
 
 
-file "#{ENV['HOME']}/Desktop/oreilly/chapter4Recipies/stone.txt" do 
+file "#{ENV['HOME']}/Desktop/oreilly/chapter4/output\ files/stone.txt" do 
 	 
 	content "Written in stone\n"
 	
 end 
 
 	
+file "#{ENV['HOME']}/Desktop/oreilly/chapter4/output\ files/hello.txt" do 
+       content 'welcome to chef'
+end
+
+
