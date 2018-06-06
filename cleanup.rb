eb01f6d2359e6818f3ed78609fd6a0e0d4f963f5
JohnWilliams1990 <JohnWilliams199032@gmail.com>
2018-06-05 19:22:01 -0600

    Merging resources to make one recipy in cleanup

---
+++ cleanup.rb
@@ -0,0 +1,13 @@
+
+
+
+file "#{ENV['HOME']}/Desktop/oreilly/chapter4Recipies/stone.txt" do 
+	action :delete
+end 
+
+
+file "#{ENV['HOME']}/Desktop/oreilly/chapter4Recipies/hello.txt" do
+        action :delete
+end
+
+
